743d2d629d6d00b8b397dbc9774909bee70030b6
Quentin <quentingraj@gmail.com>
2024-07-19 13:30:49 +0200

    ok

--- src/components/GameStatus.jsx
+++ src/components/GameStatus.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const GameStatus = ({ isGameOver, isWinner, word }) => {
   if (isGameOver) {
-    return <p className="text-red-600 mt-4">Perdu! Le mot était: {word}</p>;
+    return (
+      <p className="text-red-600 mt-4 font-bold text-2xl font-chalk">Ah ! Vous avez perdu !<br/> Le mot était {word}</p>
+    );
   }
   if (isWinner) {
-    return <p className="text-green-600 mt-4">Gagné! Félicitations!</p>;
+    return <p className="text-green-600 mt-4 text-2xl font-bold font-chalk">Mince ! <br/> Vous avez trouvé le mot. </p>;
   }
   return null;
 };
--- src/components/WrongLetters.jsx
+++ src/components/WrongLetters.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const WrongLetters = ({ wrongGuesses }) => {
   return (
     <div className="text-red-600 mb-4">
-      <p className='text-white font-chalk'>Erreurs: {wrongGuesses}</p>
+      <p className="text-white font-chalk text-xl">Erreurs: {wrongGuesses}</p>
     </div>
   );
 };
