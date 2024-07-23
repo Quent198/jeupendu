a653d9adad8d9776e57d5eb194c5b63303584586
Quentin <quentingraj@gmail.com>
2024-07-23 14:36:18 +0200

    ok

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
+
   function handleSubmit(e) {
     e.preventDefault();
     const input = document.getElementsByClassName("input-username")[0].value;
@@ -20,7 +21,7 @@ function Home() {
         backgroundSize: "cover",
         backgroundImage: "url('/chalkboard.jpg')",
       }}
-      className="min-h-screen flex flex-col items-center  p-4 py-10 w-screen relative justify-center gap-10"
+      className="min-h-screen flex flex-col items-center p-4 py-10 w-screen relative justify-center gap-10"
     >
       <h1 className="text-4xl font-bold mb-4 text-white font-chalk">
         Jeu du Pendu
@@ -31,45 +32,40 @@ function Home() {
       </p>
       <main className="flex gap-10 sm:flex-row flex-col">
         {!localStorage.getItem("username") ? (
-          <form className="flex flex-col gap-1" action="">
-            {/* Open the modal using document.getElementById('ID').showModal() method */}
-            <button 
-              className="btn  font-chalk w-[15rem] hover:cursor-pointer"
-              onClick={(e) => {
-                e.preventDefault(),
-                  document.getElementById("my_modal_1").showModal();
-              }}
-            >
+          <>
+            <label htmlFor="my_modal_7" className="btn font-chalk w-[15rem] hover:cursor-pointer">
               Se connecter
-            </button>
-            <dialog id="my_modal_1" className="modal">
+            </label>
+            <input type="checkbox" id="my_modal_7" className="modal-toggle" />
+            <div className="modal" role="dialog">
               <div className="modal-box">
-                
-                
-                <input className="input-username border-2 h-10 w-full" placeholder="pseudo" type="text" />
+                <input
+                  className="input-username border-2 h-10 w-full"
+                  placeholder="pseudo"
+                  type="text"
+                />
                 <div className="modal-action">
-                  <form className="w-full" method="dialog">
-                    {/* if there is a button in form, it will close the modal */}
-                    <button
-                      className="btn  font-chalk  w-full"
-                      onClick={(e) => {
-                        handleSubmit(e);
-                      }}
-                    >
-                      Se connecter
-                    </button>
-                  </form>
+                  <label
+                    htmlFor="my_modal_7"
+                    className="btn font-chalk w-full"
+                    onClick={(e) => {
+                      handleSubmit(e);
+                    }}
+                  >
+                    Se connecter
+                  </label>
                 </div>
               </div>
-            </dialog>
-          </form>
+              <label className="modal-backdrop" htmlFor="my_modal_7">Close</label>
+            </div>
+          </>
         ) : (
           <>
             <button
               onClick={() => {
                 navigate("/game");
               }}
-              className="btn  font-chalk w-[15rem] "
+              className="btn font-chalk w-[15rem]"
             >
               Commencer le jeu
             </button>
@@ -80,7 +76,7 @@ function Home() {
           onClick={() => {
             navigate("/leaderboard");
           }}
-          className="btn font-chalk  w-[15rem]"
+          className="btn font-chalk w-[15rem]"
         >
           Découvrir le classement
         </button>
@@ -90,3 +86,4 @@ function Home() {
 }
 
 export default Home;
+
